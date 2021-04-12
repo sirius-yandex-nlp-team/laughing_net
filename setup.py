62248478f8d45e5c1f5260c0770a4c3579122af7
Ivan Lazunin <code.wibbn@gmail.com>
2021-04-12 18:31:16 +0300

    add params and requirements

--- setup.py
+++ setup.py
@@ -3,5 +3,23 @@ import setuptools
 setuptools.setup(
     name="laughing_net",
     packages=setuptools.find_packages(),
-    install_requires=[],
+    install_requires=[
+        'pytorch-pretrained-bert',
+        'tqdm',
+        'boto3',
+        'requests',
+        'regex',
+        'pytorch-nlp',
+        'sacremoses',
+        'sentencepiece',
+        'pytorch_transformers',
+        'transformers',
+        'gensim',
+        'spacy',
+        'python-box',
+        'scikit-learn',
+        'keras-preprocessing',
+        'pandas',
+        'click'
+    ],
 )
