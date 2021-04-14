0c5a3b6b35a519677c92f103e0f2c02287212298
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2021-04-14 12:00:42 +0300

    add train_rugpt_anek stage (draft)

---
+++ laughing_net/models/rugpt.py
@@ -0,0 +1,53 @@
+import click
+
+from transformers import (
+    AutoTokenizer, 
+    AutoModelForCausalLM, 
+    DataCollatorForLanguageModeling,
+    pipeline, 
+)
+from datasets import load_dataset
+
+from src.context import ctx
+from src.config import params
+
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.option("--train-name", type=str)
+@click.option("--test-name", type=str)
+@click.option("--train-type", type=str, default="text")
+@click.option("--test-type", type=str, default="text")
+def train(train_name, test_name, train_type, test_type):
+    rugpt_params = params.models.rugpt
+    train_params = rugpt_params.stages.train
+    train_dataset = load_dataset(train_type, data_files=ctx.data_dir / "processed" / train_name)
+    test_dataset = load_dataset(test_type, data_files=ctx.data_dir / "processed" / test_name)
+    tokenizer = AutoTokenizer.from_pretrained(rugpt_params.name)  
+    model = AutoModelForCausalLM.from_pretrained(rugpt_params.name)
+    data_collator = DataCollatorForLanguageModeling(
+        tokenizer=tokenizer, mlm=False,
+    )
+    training_args = TrainingArguments(
+        output_dir=ctx.root_dir / "artifacts" / rugpt_params.checkpoint_name,
+        overwrite_output_dir=True,
+        **train_params,
+    )
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        data_collator=data_collator,
+        train_dataset=train_dataset,
+        eval_dataset=test_dataset,
+        prediction_loss_only=True,
+    )
+    trainer.train()
+
+@cli.command()
+def generate():
+    pass
+
+if __name__ == "__main__":
+    cli()
